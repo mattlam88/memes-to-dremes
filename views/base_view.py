ea2cc0bc38ec12c23046c276e7b9618b98ee0fdc
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-04-08 19:05:06 -0400

    Create abstract base classes for model, view, and controller

---
+++ views/base_view.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import abc
+from typing import Any, Optional, Type, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from controllers.base_controller import BaseController
+    from models.base_model import BaseModel
+
+
+class BaseView(metaclass=abc.ABCMeta):
+    """App view base class."""
+
+    @classmethod
+    def __subclasshook__(cls, subclass) -> bool:
+        return (hasattr(subclass, "_connectSignals") and callable(subclass._connectSignals) or
+                NotImplemented)
+
+    def __init__(self, model, controller, ui=None) -> None:
+        super().__init__()
+
+        self._model: Type[BaseModel] = model
+        self._controller: Type[BaseController] = controller
+        self._ui = ui
+
+        if self.ui is not None:
+            self.ui.setupUi(self)
+
+        self._connectSignals()
+
+    @property
+    def model(self) -> Type[BaseModel]:
+        return self._model
+
+    @property
+    def controller(self) -> Type[BaseController]:
+        return self._controller
+
+    @property
+    def ui(self) -> Optional[Any]:
+        return self._ui
+
+    @abc.abstractmethod
+    def _connectSignals(self) -> None:
+        raise NotImplementedError
