915ec457dec98face26ebe55a7658408414dd4f3
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-04-09 15:42:20 -0400

    Add twitter streaming functionality to app class
    App will run an instance of TwitterStream on startup.

--- app.py
+++ app.py
@@ -1,16 +1,49 @@
-from PySide2.QtWidgets import QApplication
+from __future__ import annotations
 
-from controllers.dashboard_controller import DashboardController
-from models.dashboard_model import DashboardModel
-from views.dashboard_view import DashboardView
+from typing import cast, TYPE_CHECKING
+
+from PySide2.QtWidgets import QApplication
+import tweepy as tp
+
+import config
+from controllers.app_controller import AppController
+from models.app_model import AppModel
+from models.stream_listener import StreamListener
+from views.app_view import AppView
+
+if TYPE_CHECKING:
+    from tweepy import Stream
+    from controllers.base_controller import BaseController
+    from views.base_view import BaseView
+
+
+# Handles authentication by pulling API key information from config file
+auth = tp.OAuthHandler(config.API_KEY, config.API_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+api = tp.API(auth)
 
 
 class App(QApplication):
     def __init__(self, sys_argv):
         super().__init__(sys_argv)
 
-        self._model = DashboardModel()
-        self._controller = DashboardController(model=self._model)
-        self._view = DashboardView(self._model, self._controller)
+        self._model: BaseModel = AppModel()
+        self._controller: BaseController = AppController(model=self._model)
+        self._view: BaseView = AppView(self._model, self._controller)
 
-        self._view.show()
+        # run twitter stream
+        self._stream = self._subscribeToTwitterStream()
+        self._stream.filter(track=["bitcoin", "btc"], is_async=True)
+
+        cast(AppView, self._view).show()
+
+    def _subscribeToTwitterStream(self) -> Stream:
+        stream_listener: StreamListener = StreamListener()
+        stream_listener.controller = self.controller
+        stream: Stream = tp.Stream(auth=api.auth, listener=stream_listener)
+
+        return stream
+
+    @property
+    def controller(self) -> BaseController:
+        return self._controller
