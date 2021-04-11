2b9f7ac0682629384cae1d07b155dad85b3e8b79
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-04-10 23:57:07 -0400

    Create new signals and properties in app model
    Connect app view to new signals.

--- models/app_model.py
+++ models/app_model.py
@@ -1,4 +1,4 @@
-from typing import List, Type
+from typing import Dict, List, Type
 
 from PySide2.QtCore import QObject, Signal
 
@@ -12,28 +12,38 @@ class AppModelMeta(type(QObject), type(BaseModel)):
 class AppModel(QObject, BaseModel, metaclass=AppModelMeta):
     btnTextChanged: Signal = Signal(str)
     tweetHistoryChanged: Signal = Signal(list)
+    tweetAdded: Signal = Signal(dict)
+    influencerFollowed: Signal = Signal(str)
+    influencerUnFollowed: Signal = Signal(str)
 
     def __init__(self) -> None:
         QObject.__init__(self)
         BaseModel.__init__(self)
 
-        self._btnText: str = str()
-        self._tweetHistory: List[str] = list()
+        self._tweetHistory: List[Dict[str, str]] = list()
+        self._followingInfluencers: List[str] = list()
 
     @property
-    def btnText(self) -> str:
-        return self._btnText
-
-    @btnText.setter
-    def btnText(self, value: str) -> None:
-        self._btnText = value
-        self.btnTextChanged.emit(value)
-
-    @property
-    def tweetHistory(self) -> List[str]:
+    def tweetHistory(self) -> List[Dict[str, str]]:
         return self._tweetHistory
 
     @tweetHistory.setter
     def tweetHistory(self, value) -> None:
         self._tweetHistory = value
         self.tweetHistoryChanged.emit(value)
+
+    @property
+    def followingInfluencers(self) -> List[str]:
+        return self._followingInfluencers
+
+    def addTweet(self, tweet) -> None:
+        self.tweetHistory.append(tweet)
+        self.tweetAdded.emit(tweet)
+
+    def followInfluencer(self, twitterHandle: str) -> None:
+        self.followingInfluencers.append(twitterHandle)
+        self.influencerFollowed.emit(twitterHandle)
+
+    def unFollowInfluencer(self, twitterHandle: str) -> None:
+        self.followingInfluencers.remove(twitterHandle)
+        self.influencerUnFollowed.emit(twitterHandle)
